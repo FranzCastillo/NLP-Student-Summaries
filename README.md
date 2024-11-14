4eb98af2f270aecea39093020827ebb70e504432
Francisco Castillo <122114861+FranzCastillo@users.noreply.github.com>
2024-11-13 19:02:51 -0600

    Added Rouge Score

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 ## What's this?
 This is an Exploratory Data Analysis done for the Kaggle Competition 
 [CommonLit - Evaluate Student Summaries](https://www.kaggle.com/competitions/commonlit-evaluate-student-summaries/overview)
+
+## How to run the Dashboard?
+
+The code was developed using Streamlit, so you can run the dashboard by running the following command in the terminal
+**after accessing the app directory**:
+
+```bash
+streamlit run dashboard.py
+```
--- app/dashboard.py
+++ app/dashboard.py
@@ -13,15 +13,15 @@
     - Modificado el 10/11/2024
 """
 
-import streamlit as st
-import numpy as np
-import tensorflow as tf
-from transformers import TFBertForSequenceClassification, BertTokenizer
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import streamlit as st
+from rouge_score import rouge_scorer
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from transformers import TFBertForSequenceClassification, BertTokenizer
 
-#Configuración de la paleta de colores y estilos
+# Configuración de la paleta de colores y estilos
 st.set_page_config(page_title="Evaluación Automática de Resúmenes")
 st.markdown(
     """
@@ -55,41 +55,68 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-#Cargar el modelo BERT y tokenizer
+
+# Cargar el modelo BERT y tokenizer
 @st.cache_resource
 def load_model():
     model = TFBertForSequenceClassification.from_pretrained("../src/bert_summary_model")  # Ruta a la carpeta del modelo
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
     return model, tokenizer
 
+
 model, tokenizer = load_model()
 
-#Función para evaluar con BERT
+
+# Función para evaluar con BERT
 def evaluate_with_bert(model, tokenizer, text, summary):
-    #Tokenizar y preparar los datos de entrada para BERT
-    inputs = tokenizer(text + " [SEP] " + summary, return_tensors="tf", max_length=512, truncation=True, padding="max_length")
+    # Tokenizar y preparar los datos de entrada para BERT
+    inputs = tokenizer(text + " [SEP] " + summary, return_tensors="tf", max_length=512, truncation=True,
+                       padding="max_length")
     outputs = model(inputs)
-    
-    #Extraer puntuaciones de content y wording
+
+    # Extraer puntuaciones de content y wording
     scores = outputs.logits.numpy().flatten()
     return scores[0], scores[1]
 
-#Cargar datos de predicciones desde el archivo CSV
+
+# Cargar datos de predicciones desde el archivo CSV
 @st.cache_data
 def load_predictions():
-    df_predictions = pd.read_csv("../src/BERT_predictions.csv")  # Asegúrate de que el archivo esté en el mismo directorio
+    df_predictions = pd.read_csv(
+        "../src/BERT_predictions.csv")  # Asegúrate de que el archivo esté en el mismo directorio
     test_targets = df_predictions[['Real_Content', 'Real_Wording']].values
     pred_labels = df_predictions[['Predicted_Content', 'Predicted_Wording']].values
     return test_targets, pred_labels
 
-#Menú de navegación en la barra lateral
-menu_option = st.sidebar.radio("Selecciona una sección:", ["Evaluación de Resúmenes", "Desempeño de los Modelos", "Análisis de Resúmenes"])
+
+scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
+
+
+def get_rouge(text, summary):
+    scores = scorer.score(text, summary)
+    scores = {
+        'rouge1_recall': scores['rouge1'].recall,
+        'rouge1_precision': scores['rouge1'].precision,
+        'rouge1_f1': scores['rouge1'].fmeasure,
+        'rouge2_recall': scores['rouge2'].recall,
+        'rouge2_precision': scores['rouge2'].precision,
+        'rouge2_f1': scores['rouge2'].fmeasure,
+        'rougeL_recall': scores['rougeL'].recall,
+        'rougeL_precision': scores['rougeL'].precision,
+        'rougeL_f1': scores['rougeL'].fmeasure
+    }
+    return pd.DataFrame(scores, index=[0])
+
+
+# Menú de navegación en la barra lateral
+menu_option = st.sidebar.radio("Selecciona una sección:",
+                               ["Evaluación de Resúmenes", "Desempeño de los Modelos", "Análisis de Resúmenes"])
 
 if menu_option == "Evaluación de Resúmenes":
-    #Título
+    # Título
     st.markdown('<p class="title">Evaluación Automática de Resúmenes</p>', unsafe_allow_html=True)
-    
-    #Configuración de layout
+
+    # Configuración de layout
     col1, col2, col3 = st.columns([4, 4, 2])
 
     with col1:
@@ -112,38 +139,52 @@ if menu_option == "Evaluación de Resúmenes":
         st.subheader("Selección del modelo")
         model_choice = st.selectbox("Evaluar con:", ["BERT", "ROUGE", "Ambos"])
 
-    #Botón para evaluar
+    # Botón para evaluar
     if st.button("Evaluar", key="evaluate", help="Haz clic para evaluar el resumen"):
         if original_text and summary_text:
-            if model_choice in ["BERT", "Ambos"]:
-                bert_content_score, bert_wording_score = evaluate_with_bert(model, tokenizer, original_text, summary_text)
+            if model_choice == "BERT":
+                bert_content_score, bert_wording_score = evaluate_with_bert(model, tokenizer, original_text,
+                                                                            summary_text)
                 st.subheader("Puntuación BERT")
                 st.write(f"Content Score: {bert_content_score:.2f}")
                 st.write(f"Wording Score: {bert_wording_score:.2f}")
+            elif model_choice == "ROUGE":
+                rouge_scores = get_rouge(original_text, summary_text)
+                st.subheader("Puntuación ROUGE")
+                st.write(rouge_scores)
+            elif model_choice == "Ambos":
+                bert_content_score, bert_wording_score = evaluate_with_bert(model, tokenizer, original_text,
+                                                                            summary_text)
+                rouge_scores = get_rouge(original_text, summary_text)
+                st.subheader("Puntuación BERT")
+                st.write(f"Content Score: {bert_content_score:.2f}")
+                st.write(f"Wording Score: {bert_wording_score:.2f}")
+                st.subheader("Puntuación ROUGE")
+                st.write(rouge_scores)
         else:
             st.warning("Por favor, cargue o ingrese ambos textos para evaluar.")
 
 elif menu_option == "Desempeño de los Modelos":
     st.markdown('<p class="title">Desempeño de los Modelos</p>', unsafe_allow_html=True)
 
-    #Sección de selección de métrica
+    # Sección de selección de métrica
     st.sidebar.subheader("Métricas")
     metric_choice = st.sidebar.radio("Selecciona la métrica:", ["MSE", "MAE", "R²"])
 
-    #Cargar los datos de predicciones y valores reales
+    # Cargar los datos de predicciones y valores reales
     test_targets, pred_labels = load_predictions()
 
-    #Cálculo de Errores y Visualización según la Métrica Seleccionada
+    # Cálculo de Errores y Visualización según la Métrica Seleccionada
     if metric_choice == "MSE":
         mse_content = mean_squared_error(test_targets[:, 0], pred_labels[:, 0])
         mse_wording = mean_squared_error(test_targets[:, 1], pred_labels[:, 1])
         metric_description = "Esta métrica muestra el Error Cuadrático Medio, lo que significa que mide el promedio de los errores al cuadrado entre los valores reales y las predicciones."
 
-        #Mostrar valores de MSE
+        # Mostrar valores de MSE
         st.write(f"**MSE (Content):** {mse_content:.4f}")
         st.write(f"**MSE (Wording):** {mse_wording:.4f}")
 
-        #Histograma de Errores Cuadrados para MSE
+        # Histograma de Errores Cuadrados para MSE
         squared_errors_content = np.square(test_targets[:, 0] - pred_labels[:, 0])
         squared_errors_wording = np.square(test_targets[:, 1] - pred_labels[:, 1])
 
@@ -165,11 +206,11 @@ elif menu_option == "Desempeño de los Modelos":
         mae_wording = mean_absolute_error(test_targets[:, 1], pred_labels[:, 1])
         metric_description = "Esta métrica muestra el Error Absoluto Medio, que representa el promedio de los errores absolutos entre los valores reales y las predicciones."
 
-        #Mostrar valores de MAE
+        # Mostrar valores de MAE
         st.write(f"**MAE (Content):** {mae_content:.4f}")
         st.write(f"**MAE (Wording):** {mae_wording:.4f}")
 
-        #Histograma de Errores Absolutos para MAE
+        # Histograma de Errores Absolutos para MAE
         abs_errors_content = np.abs(test_targets[:, 0] - pred_labels[:, 0])
         abs_errors_wording = np.abs(test_targets[:, 1] - pred_labels[:, 1])
 
@@ -186,16 +227,16 @@ elif menu_option == "Desempeño de los Modelos":
         ax_mae_wording.set_ylabel("Frecuencia")
         st.pyplot(fig_mae)
 
-    else:  #R^2
+    else:  # R^2
         r2_content = r2_score(test_targets[:, 0], pred_labels[:, 0])
         r2_wording = r2_score(test_targets[:, 1], pred_labels[:, 1])
         metric_description = "Esta métrica muestra el Coeficiente de Determinación (R²), que indica qué tan bien se ajustan las predicciones a los valores reales."
 
-        #Mostrar valores de R^2
+        # Mostrar valores de R^2
         st.write(f"**R² (Content):** {r2_content:.4f}")
         st.write(f"**R² (Wording):** {r2_wording:.4f}")
 
-        #Histograma de Residuos para R^2
+        # Histograma de Residuos para R^2
         residuals_content = test_targets[:, 0] - pred_labels[:, 0]
         residuals_wording = test_targets[:, 1] - pred_labels[:, 1]
 
@@ -212,9 +253,9 @@ elif menu_option == "Desempeño de los Modelos":
         ax_r2_wording.set_ylabel("Frecuencia")
         st.pyplot(fig_r2)
 
-    #Mostrar la descripción de la métrica seleccionada
+    # Mostrar la descripción de la métrica seleccionada
     st.sidebar.write(metric_description)
 
 elif menu_option == "Análisis de Resúmenes":
-    #Título
+    # Título
     st.markdown('<p class="title">Análisis de Resúmenes</p>', unsafe_allow_html=True)
